98409afd2a152bb36d55152283c5b6f69e469813
your name <abukarakiomar@gmail.com>
2024-06-10 11:57:17 +0300

    finish 7 day

--- 7 day/exercises_level_3.js
+++ 7 day/exercises_level_3.js
@@ -229,4 +229,119 @@
 
 
 // 14-
+// function modifyArray(arr){
+//     if(arr.length>=5){
+//         let newArr =[];
+//         for (let index = 0; index < arr.length; index++) {
+//             if(index === 4){
+//                 newArr.push(arr[index].toUpperCase());
+//             }else{
+//                 newArr.push(arr[index]);
+//             }
+//         }
+//         return newArr;
+//     }else{
+//         return "'Not Found'";
+//     }
+// }
+// console.log(modifyArray(['Avocado', 'Tomato', 'Potato','Mango', 'Lemon','Carrot']));
+// console.log(modifyArray(['Google', 'Facebook','Apple', 'Amazon','Microsoft',  'IBM']));
+// console.log(modifyArray(['Google', 'Facebook','Apple', 'Amazon']));
 
+
+// 15-
+// function isPrime(number){
+//     if(number < 1){
+//         return false;
+//     }
+//     else{
+//         for (let i = 2; i <= Math.sqrt(number); i++) {
+//             if (number % i === 0) {
+//               return false;
+//             }
+//           }
+//           return true;
+//     }
+  
+// }
+// console.log(isPrime(11));
+
+
+// 16-
+// function checksAllItemUnique(arr){
+//     let counter = 0;
+//    for (let index = 0; index < arr.length; index++) {
+//         for (let i = 0; i < arr.length; i++) {
+//              if(arr[index] === arr[i]){
+//                 counter++;
+//              }            
+//         }    
+//    }
+//    if(counter === arr.length){
+//      return 'All items are unique in the array.';
+//    }else{
+//     return 'There is same item in the array.';
+//    }
+// }
+// console.log(checksAllItemUnique(['one','two','three']));
+// console.log(checksAllItemUnique([1,3,2,3]));
+
+
+// 17-
+// function checkSameDataType(arr){
+//   let counter = 0;
+//   let item = arr[0];
+//   for (let index = 0; index < arr.length; index++) {
+//        if(typeof item === typeof arr[index]){
+//         counter++;
+//        }    
+//   }
+//   if(counter === arr.length){
+//     return 'Same item';
+//   }else{
+//     return 'Not same item';
+//   }
+// }
+// console.log(checkSameDataType([1,2,'3',4]));
+// console.log(checkSameDataType(['one','two','three']));
+// console.log(checkSameDataType(['one',false,'two','three']));
+
+
+// 18-
+// function isValidVariable(variableName){
+//     let vN = variableName.match(/[!@#%^&*()-+\|{}:;/?._=]/g);
+//     if(vN != null){
+//         if(vN.length > 0){
+//         return false;
+//     }else{
+//         return true;
+//     }
+//     }else{
+//         return true;
+//     }
+   
+// }
+// console.log(isValidVariable('on!e%'));
+// console.log(isValidVariable('one'));
+
+
+// 19-
+// function sevenRandomNumbers(){
+//     let numbers = [0,1,2,3,4,5,6,7,8,9];
+//     let randomList = [];
+//    for (let index = 0; index < 7; index++) {
+//     let randomNumber = Math.floor(Math.random() * numbers.length);
+//     randomList.push(numbers[randomNumber]);
+//     numbers.splice(randomNumber,1);
+//    }
+//    return randomList;
+// }
+// console.log(sevenRandomNumbers());
+
+
+// 20-
+// function reverseCountries(arr){
+//     console.log(arr);
+//     console.log(arr.reverse());
+// }
+// reverseCountries(['island','ferland','jordan']);
