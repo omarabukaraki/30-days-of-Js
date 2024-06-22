069e8cbb65c5df9d9057372660168c282465a6d0
your name <abukarakiomar@gmail.com>
2024-06-22 12:05:45 +0300

    finish 16 day

---
+++ 16_day/exercises_level_2.js
@@ -0,0 +1,10 @@
+const student = {
+    firstName:'Asabeneh',
+    lastName:'Yetayehe',
+    age:250,
+    isMarried:true,
+    skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+  }
+
+const studentText = JSON.stringify(student,['firstName','lastName','skills'],1);
+console.log(studentText);
