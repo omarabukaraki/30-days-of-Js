75f298065ed7789fc2ad4a37b47ed7cf4112b774
your name <abukarakiomar@gmail.com>
2024-06-29 12:36:55 +0300

    finish day 28

--- 28_day__leader_board/main.js
+++ 28_day__leader_board/main.js
@@ -12,10 +12,13 @@ const nameDateDate = document.querySelector('.nameDate .date');
 const country =document.querySelector('.country');
 const scoreNumber =document.querySelector('.scoreNumber');
 const buttons = document.querySelector('.buttons');
-const btn = document.querySelectorAll('.btn');
+let addBtn = document.querySelectorAll('.addBtn');
+let removeBtn = document.querySelectorAll('.removeBtn');
+let deleteBtn = document.querySelectorAll('.deleteBtn');
+const fieldsMessage = document.querySelector('.fieldsMessage');
 
 
-//start style the element
+// //start style the element
 document.body.style.cssText ='display: flex;justify-content: center;';
 content.style.cssText = 'width: 80%;text-align: center;';
 title.style.cssText = 'font-weight: 500;font-size: 26px;margin-top: 50px;';
@@ -23,6 +26,7 @@ fromPlayer.style.cssText = 'margin: 40px 0px;';
 fromPlayerInput.forEach(function(inputFeild){
     inputFeild.style.cssText = 'width: 16%;height: 40px;padding-left: 10px;border: 1px solid #e5c9c5;';});
 fromPlayerButton.style.cssText = 'background-color: #e5c9c5;width: 16%;height: 44px;margin: 0;border: none;color: white;';
+
 playersScore.style.cssText = 'width: 100%;justify-content: center;display: flex;flex-direction: column;align-items: center;';
 score.style.cssText = 'justify-content: space-between;align-items: center;flex-direction: row;width: 85%;margin: 10px 0px;background-color: #e5c9c5;display: flex;';
 nameDateDate.style.cssText = ' color: gray;font-size:12px;margin:8px';
@@ -31,13 +35,142 @@ nameDate.style.cssText = 'text-align: start;margin:8px 16px;';
 country.style.cssText = ' font-size: 16px;';
 scoreNumber.style.cssText = ' font-size: 16px;';
 buttons.style.cssText = 'width: 25%;height: 100%;display: flex;align-items: center;justify-content: space-evenly; ';
-btn.forEach((btnD)=>{
-    btnD.style.cssText = 'border-radius: 100px;height: 50px;width: 50px;background-color: aliceblue;display: flex; align-items: center;justify-content: center;';
+
+const btnStyle = 'border-radius: 100px;height: 50px;width: 50px;background-color: aliceblue;display: flex; align-items: center;justify-content: center;';
+deleteBtn.forEach((deleBtn)=>{
+    deleBtn.style.cssText = `${btnStyle}color:red`;
 });
-//end style the element
+addBtn.forEach((addB)=>{
+    addB.style.cssText = btnStyle;
+});
+removeBtn.forEach((remBtn)=>{
+    remBtn.style.cssText = btnStyle;
+   });
+   fieldsMessage.style.cssText = 'color: red;display: none;';
+// //end style the element
 
 
 
+
+
+let users = JSON.parse(localStorage.getItem('users')) || [];
+let dateNow = new Date();
+let dateWithFormat = `${dateNow.getDate()}/${dateNow.getMonth()+1}/${dateNow.getFullYear()}`;
+
+
+
+fromPlayerButton.onclick = function(event) {
+    event.preventDefault();
+
+   let inputL = 0;
+   fromPlayerInput.forEach((userInput)=>{
+    if(userInput.value !== ''){
+        inputL++;
+    }else{
+        userInput.style.border = '1px solid red'; 
+    }
+   });
+   
+   if(fromPlayerInput.length === inputL){
+    let userData = {
+        firstName: fromPlayerInput[0].value,
+        lastName: fromPlayerInput[1].value,
+        country: fromPlayerInput[2].value,
+        playersScore: fromPlayerInput[3].value,
+        date:dateWithFormat,
+        userIndex:users.length,
+    };
+
+    users.push(userData);
+    localStorage.setItem('users', JSON.stringify(users));
+    
+    fromPlayerInput.forEach(input => input.value = '');
+    getAndCreate();
+    location.reload();
+   }else{
+    fieldsMessage.style.display = 'block';
+
+   }
+
+
+};
+
+function getAndCreate() {
+    playersScore.innerHTML = ''; 
+
+    let usersDataFromLocal = JSON.parse(localStorage.getItem('users')) || [];
+    usersDataFromLocal.sort((a,b)=>b.playersScore - a.playersScore).forEach(function(user,index) {
+        let copyScore = score.cloneNode(true);
+        playersScore.appendChild(copyScore);
+
+        let scoreAll = document.querySelectorAll('.score');
+        scoreAll[index].children[0].children[0].innerHTML = user.firstName + " " + user.lastName;
+        scoreAll[index].children[0].children[1].innerHTML = user.date;
+        scoreAll[index].children[1].textContent = user.country;
+        scoreAll[index].children[2].textContent = user.playersScore;
+    });
+    deleteBtn = document.querySelectorAll('.deleteBtn');
+    addBtn = document.querySelectorAll('.addBtn');
+    removeBtn = document.querySelectorAll('.removeBtn');
+}
+
+getAndCreate();
+
+
+
+deleteBtn.forEach((b,index)=>{
+    b.onclick = function(){
+         users = JSON.parse(localStorage.getItem('users')) || [];
+         users.sort((a,b)=>b.playersScore - a.playersScore);
+        users.splice(index,1);
+        localStorage.setItem('users', JSON.stringify(users));
+        location.reload();
+    }
+    b.addEventListener('mouseover',()=>{
+        b.style.cssText = 'cursor: pointer;color:red;border-radius: 100px;height: 50px;width: 50px;background-color: #def4b3;display: flex; align-items: center;justify-content: center;';
+
+    });
+    b.addEventListener('mouseout',()=>{
+        b.style.cssText = `${btnStyle}color:red`;
+    });
+});
+
+addBtn.forEach((b,index)=>{
+    b.onclick = function(){
+         users = JSON.parse(localStorage.getItem('users')) || [];
+         users.sort((a,b)=>b.playersScore - a.playersScore);
+        users[index].playersScore=  (+users[index].playersScore)+5;
+        localStorage.setItem('users', JSON.stringify(users));
+        location.reload();
+    }
+    b.addEventListener('mouseover',()=>{
+        b.style.cssText = 'cursor: pointer;border-radius: 100px;height: 50px;width: 50px;background-color: #def4b3;display: flex; align-items: center;justify-content: center;';
+
+    });
+    b.addEventListener('mouseout',()=>{
+        b.style.cssText = btnStyle;
+    });
+});
+
+removeBtn.forEach((b,index)=>{
+    b.addEventListener('click',()=>{
+        users = JSON.parse(localStorage.getItem('users')) || [];
+        users.sort((a,b)=>b.playersScore - a.playersScore);
+        if(users[index].playersScore >= 5){
+            users[index].playersScore=  (+users[index].playersScore)-5;
+        localStorage.setItem('users', JSON.stringify(users));
+        location.reload();
+        }
+    });
+    b.addEventListener('mouseover',()=>{
+        b.style.cssText = 'cursor: pointer;border-radius: 100px;height: 50px;width: 50px;background-color: #def4b3;display: flex; align-items: center;justify-content: center;';
+
+    });
+    b.addEventListener('mouseout',()=>{
+        b.style.cssText = btnStyle;
+    });
+
+});
 
 
 
